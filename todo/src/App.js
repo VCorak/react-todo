f8809581526eb5341c679fbca90acc2593d6ffd6
Valentina Corak <valentinacorak@gmail.com>
2021-07-06 16:46:25 +0200

    Added some styling and made new component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import './App.css';
 import TodoList from "./TodoList";
 
 function App() {
   return (
-      <>
-        <TodoList />
-        <input type = "text" />
-          <button>Add Todo</button>
-          <button>Clear Todos</button>
-          <div>0 left to do</div>
-      </>
+
+      <div className='todo-app'>
+          <h1> TODO APP</h1>
+          <form className='todo-form'>
+        <input placeholder='Add a todo' type= "text"
+        className='todo-input'/>
+          <button className='add-todo-button'>Add todos</button>
+          <button className='clear-todo-button'>Clear todos</button>
+          </form>
+          <div className='left-todo'>0 left to do</div>
+          <TodoList />
+      </div>
   );
 }
 
--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Todo from "./Todo";
+import './App.css';
 
 function TodoList() {
     return (
         <div>
             <li>Learn components</li>
             <li>Learn hooks</li>
+            <li>Add styling</li>
         </div>
     );
 };
