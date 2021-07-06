c467ae7ef0111c6b7d380290c01143badc182178
Valentina Corak <valentinacorak@gmail.com>
2021-07-06 15:31:12 +0200

    Added new components

---
+++ todo/src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from "./Todo";
+
+function TodoList() {
+    return (
+        <div>
+            <li>Learn components</li>
+            <li>Learn hooks</li>
+        </div>
+    );
+};
+
+export default TodoList;
